4315a3146e226b99915b1dba9a114b1db9cc220a
Daniil Azbukin <azbukin2003@gmail.com>
2024-01-10 00:37:45 +0300

    js

--- WorkoutFit/assets/scripts/authorization_input.js
+++ WorkoutFit/assets/scripts/authorization_input.js
@@ -1,17 +1,28 @@
-
-$("#login-input").focus(function(){
-    $(".login-input-container").toggleClass("focused");
-});
-
-$("#login-input").focusout(function(){
-    $(".login-input-container").toggleClass("focused");
-});
+let inputs = ["#login-input", "#password-input", "#first-name-input", "last-name-input"];
+let containers = [".login-input-container", ".password-input-container", ".first-name-input-container",".last-name-input-container"];
 
 
-$("#password-input").focus(function(){
-    $(".password-input-container").toggleClass("focused");
-});
-
-$("#password-input").focusout(function(){
-    $(".password-input-container").toggleClass("focused");
-});
+for (let i = 0; i < inputs.length; i++) {
+    $(inputs[i]).focus(function(){
+        $(containers[i]).toggleClass("focused");
+    });
+    
+    $(inputs[i]).focusout(function(){
+        $(containers[i]).toggleClass("focused");
+    });
+};
+$("#first-name-input").on('keyup input', function(){
+    var value = this.value;
+    tick(".first-name-input-container", /^[a-zA-Z0-9А-Яа-я]+$/.test(value));
+})
+$("#last-name-input").on('keyup input', function(){
+    var value = this.value;
+    tick(".last-name-input-container", /^[a-zA-Z0-9А-Яа-я]+$/.test(value));
+})
+function tick(container,flag){
+    if (flag) {
+        $(container+" svg").css("opacity", "1");
+    } else{
+        $(container+" svg").css("opacity", "0");
+    }
+}
